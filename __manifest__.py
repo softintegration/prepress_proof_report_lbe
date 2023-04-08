fcae7d503e9563cdae1e1b771966e2c626ecb387
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-04-08 09:29:41 +0100

    [IMP] Prepress proof:add the line that separates the grammage and folding dimensions fields

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     'qweb': [],
     'description': False,
     'images': [],
-    'version': '1.0.1.7',
+    'version': '1.0.1.8',
     'category': 'Prepress/Prepress',
     'demo': [],
     'depends': ['prepress_management','product_quality'],
